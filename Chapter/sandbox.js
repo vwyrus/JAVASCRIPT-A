45e36f2bf519bf597c3e01f447dccf0de5a00a04
vwyrus <oputuogheneyoma@gmail.com>
2024-01-31 01:42:32 +0100

    chapter 1

---
+++ Chapter/sandbox.js
@@ -0,0 +1,15 @@
+// alert('hello, world');
+let age = 25;
+let year = 2019;
+
+console.log(age, year);
+
+age = 70;
+console.log(age);
+
+
+const points = 100;
+console.log(points);
+
+var score = 60;
+console.log(score);
