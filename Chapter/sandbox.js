3c91b253d82e899423adc70a00a5777ec580e1e9
vwyrus <oputuogheneyoma@gmail.com>
2024-02-01 14:02:45 +0100

    Fundamenals of Javascript

--- Chapter/sandbox.js
+++ Chapter/sandbox.js
@@ -1,4 +1,5 @@
 // alert('hello, world');
+/*
 let age = 25;
 let year = 2019;
 
@@ -13,3 +14,128 @@ console.log(points);
 
 var score = 60;
 console.log(score);
+
+//Data types
+//strings
+console.log('hello, world');
+
+let email = 'mario@thenetninja.co.uk'
+console.log(email);
+
+//string concatenation
+let firstName = 'Brandon';
+let lastName = 'Sanderson';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[2]);
+
+//string length
+console.log(fullName.length);
+
+// string methods
+// console.log(fullName.toUpperCase());
+// let result = fullName.toLowerCase();
+// console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index);
+
+//common string methods
+
+// let result = email.lastIndexOf('n');
+// let result = email.slice(0, 5);
+// let result = email.substr(4, 10);
+let result = email.replace('n', 'w');
+console.log(result);
+*/
+
+
+// let radius = 10;
+// const pi = 3.14;
+
+// let result = radius % 3;
+// let result = pi * radius ** 2;
+
+// BIDMAS
+/* likes ++, --, +=, -=, *=, /=;
+*/
+
+// NAN - not a number 
+
+// const title = 'Best reads of 2019';
+// const author = 'Mario';
+// const likes = 30;
+// // concantenatiom way
+// let result = `The blog called ${title} by ${author} has ${likes} likes`;
+// console.log(result);
+
+// // creating html templates
+// let html = `
+//   <h2>${title}</h2>
+//   <p>By ${author}</p>
+//   <span>This blog has ${likes} likes</span>
+// `;
+// console.log(html);
+let ninjas = ['shaun', 'ryu', 'chun-li'];
+// ninjas[1] = 'ken';
+// console.log(ninjas[1]);
+// console.log(ninjas.length);
+
+// let ages = [20, 25, 30, 35];
+// console.log(ages[3]);
+
+// let random = ['shaun', 'crystal', 30, 20];
+// console.log(random);
+
+// array methods
+// let result = ninjas.join(', ');
+// let result = ninjas.indexOf('chun-li');
+// let result = ninjas.concat(['ken', 'crystal']);
+// let result = ninjas.push('ken');
+// result = ninjas.pop();
+// console.log(result);
+
+
+console.log(true, false, "true", "false");
+
+// methods can return booleans
+let email = 'luigi@thenetninja.co.uk';
+let names = ['mario', 'luigi', 'toad'];
+
+// let result = email.includes('@')
+// let result = names.includes('luigi')
+
+// comparison operators
+let age = 25;
+
+// loose comparison
+
+// console.log(age == 25);
+// console.log(age == 30);
+// console.log(age != 30);
+// console.log(age > 20);
+// console.log(age <= 25);
+// console.log(age == '25');
+// console.log(age != '25');
+
+// strict comparison (different types cannot be equal)
+// console.log(age === '25');
+// console.log(age !== '25');
+
+// let name = 'shaun';
+// console.log(name > 'crysatal');
+
+let score = '100';
+// score = Number(score);
+// console.log(typeof score);
+// console.log(score + 1);
+
+// let result = Number('hello');
+// let result = String(123);
+// let result = Boolean(1);
+let result = Boolean('')
+
+console.log(result, typeof result);
