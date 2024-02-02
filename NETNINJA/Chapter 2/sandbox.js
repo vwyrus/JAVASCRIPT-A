a52ae2572780878917fb74f6bc28dec3727c536f
vwyrus <oputuogheneyoma@gmail.com>
2024-02-02 10:34:33 +0100

    loops; for and while

---
+++ NETNINJA/Chapter 2/sandbox.js
@@ -0,0 +1,38 @@
+// for loops
+
+// for (let i = 0; i < 5; i++) {
+//   console.log('in loop:', i);
+// }
+
+// console.log('loop finished')
+
+const names = ['shaun', 'mario', 'luigi'];
+
+// for (let i = 0; i < names.length; i++) {
+//   // console.log(names[i]);
+
+//   let html = `<div>${names[i]}</div>`;
+//   console.log(html)
+// }
+
+// while loop
+
+// let i = 0;
+// while (i < 5) {
+//   console.log(`in loop: `, i);
+//   i++;
+// }
+// let i = 0;
+// while (i < names.length) {
+//   console.log(names[i]);
+//   i++;
+// }
+// let i = 2;
+
+// do {
+//   console.log('val of i is:', i)
+//   i++;
+// }
+// while (i < 5)
+
+//if statements
