aabedc5279d1c2ca04c9ec9d703029c265fcabc8
vwyrus <oputuogheneyoma@gmail.com>
2024-02-04 21:31:06 +0100

    objects literals

---
+++ NETNINJA/chapter 4/sandbox.js
@@ -0,0 +1,15 @@
+// objects literal
+
+let user = {
+  name: 'crystal',
+  age: 30,
+  email: 'crystal@thenetninja.co.uk',
+  location: 'berlin',
+  blogs: ['why mac & cheese rules', '10 things tomake with marmite']
+};
+console.log(user);
+console.log(user.name)
+
+console.log(user['email']);
+user['name'] = 'yoma'
+console.log(user['name']);
