55c2cc434f60e889a4a4b8d140161e666764ea30
vwyrus <oputuogheneyoma@gmail.com>
2024-02-05 22:47:29 +0100

    DOM

---
+++ NETNINJA/chapter 5/sandbox.js
@@ -0,0 +1,17 @@
+// const para = document.querySelector('.error');
+// console.log(para);
+
+// const paras = document.querySelectorAll('p');
+// const errors = document.querySelectorAll('.error');
+
+// console.log(paras);
+
+// get an element by ID
+// const title = document.getElementById('page-title');
+// console.log(title);
+
+// get elements by this class name
+// const errors = document.getElementsByClassName('error');
+// console.log(errors);
+
+// get elements by tag name 
